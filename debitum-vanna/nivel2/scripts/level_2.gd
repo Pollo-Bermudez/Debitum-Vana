995f3b53aad5bb07fda014f503489318b82c928e
jangel234 <jaguilar57@ucol.mx>
2025-11-12 10:57:30 -0600

    si

--- debitum-vanna/nivel2/scripts/level_2.gd
+++ debitum-vanna/nivel2/scripts/level_2.gd
@@ -8,4 +8,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("esc"):
+		_on_button_pressed()
+
+
+
+func _on_button_pressed() -> void:
+	$CanvasLayer2.visible = true
+	$CanvasLayer.visible = false
+	get_tree().paused = !get_tree().paused
+	
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().paused = !get_tree().paused
+	get_tree().change_scene_to_file("res://menu_inicial/menu.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	$CanvasLayer2.visible = false
+	$CanvasLayer.visible = true
+	get_tree().paused = !get_tree().paused
--- debitum-vanna/scenes/level_1.gd
+++ debitum-vanna/scenes/level_1.gd
@@ -35,6 +35,8 @@ func _ready():
 func _process(delta):
 	if player:
 		check_player_fall()
+	if Input.is_action_just_pressed("esc"):
+		_on_button_pressed()
 
 func check_player_fall():
 	# Si el jugador cae por debajo del límite del mapa
@@ -113,3 +115,21 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	pass # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	$CanvasLayer2.visible = true
+	$CanvasLayer.visible = false
+	get_tree().paused = !get_tree().paused
+	
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().paused = !get_tree().paused
+	get_tree().change_scene_to_file("res://menu_inicial/menu.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	$CanvasLayer2.visible = false
+	$CanvasLayer.visible = true
+	get_tree().paused = !get_tree().paused
